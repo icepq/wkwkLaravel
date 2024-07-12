aff70de5f859d5b242962db1c5aa46a1df13c506
Piceq <pmiyamiyaq@icloud.com>
2024-07-12 17:46:00 +0900

    ヘッダー作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>wkwkLaravel</title>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>WKKW</title>
     </head>
     <body>
-        あああ
-    </body>
+    @extends('layouts.app')
+    
+
+</div>
+
+</body>
 </html>
