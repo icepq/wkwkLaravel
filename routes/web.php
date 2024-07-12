d148d5309d7f9972fbaa7b0b92a64ad686ff96da
Piceq <pmiyamiyaq@icloud.com>
2024-07-13 00:18:47 +0900

    fullCalendar導入

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/add', function () {
+    return view('add');
+});
+Route::get('/calendar', function () {
+    return view('calendar');
+});
+Route::get('/discription', function () {
+    return view('discription');
+});
+Route::get('/save', function () {
+    return view('save');
+});
