82a593adca1f17f77bae0cf16e7591a02e5761ed
Piceq <pmiyamiyaq@icloud.com>
2024-07-22 19:12:51 +0900

    indexにカレンダー

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,19 @@
 
 <!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>WKKW</title>
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="{{ asset('fullcalendar-6.1.14/dist/index.global.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/google-calendar@6.1.14"></script>
+    <link rel="stylesheet" href="{{ asset('css/calendar.css') }}">
+</head>
 <body>
     @extends('layouts.app')
-    
-
-    index
+    @section('content')
+        <div id="calendar"></div>
+    @endsection
+    <script src="{{ asset('js/calendar.js') }}"></script>
 </body>
 </html>
