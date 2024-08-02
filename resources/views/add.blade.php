85f79b74618fe0fe58395fcffc46996206e5ca8b
Piceq <pmiyamiyaq@icloud.com>
2024-08-03 05:52:32 +0900

    create thanks page

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -6,10 +6,21 @@
     <title>予定を追加する</title>
     <link rel="stylesheet" href="{{ asset('css/addtuar.css') }}">
 </head>
+<script type="text/javascript">
+    let submitted = false;
+</script>
+<iframe
+    name="hidden_iframe"
+    id="hidden_iframe"
+    style="display: none"
+    onload="if(submitted){window.location='{{ url('/thanks') }}';}"
+></iframe>
 <body>
+    @extends('layouts.app')
+    @section('content')
     <div class="container">
         <h1>予定を追加する</h1>
-        <form action="https://docs.google.com/forms/d/e/1FAIpQLSfXpILrFamfJlp7NMh0kus1zDu03DSu8hzD-HUlWSVaEpP1xQ/formResponse" method="GET">
+        <form action="https://docs.google.com/forms/d/e/1FAIpQLSfXpILrFamfJlp7NMh0kus1zDu03DSu8hzD-HUlWSVaEpP1xQ/formResponse" method="POST" target="hidden_iframe" onsubmit="submitted=true;" class="c-form">
             <div class="form-group">
                 <label for="account_name">アカウント名</label>
                 <input type="text" id="account_name" name="entry.892107423" required>
@@ -38,10 +49,11 @@
             </div>
             <div class="form-group">
                 <label for="comments">その他コメント</label>
-                <textarea id="comments" name="entry.404331169" rows="4" required></textarea>
+                <textarea id="comments" name="entry.404331169" rows="4"></textarea>
             </div>
             <button type="submit">追加する</button>
         </form>
     </div>
+    @endsection
 </body>
 </html>
